ada57d048cb5237b83e6a7315f26e0eee4a2a3b3
Sachin Sachan <sachinsachan722@gmail.com>
2025-01-06 01:26:01 +0530

    readme updated

--- collectdata.py
+++ collectdata.py
@@ -1,5 +1,7 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="lu08WwdIJ89fRfTh16mT")
+import os 
+
+rf = Roboflow(api_key=os.getenv('API_KEY'))
 project = rf.workspace("kanwal-masroor-gv4jr").project("yolov7-license-plate-detection")
 version = project.version(3)
 dataset = version.download("yolov8")
